a00afd981932d1488227835778e992feffaed324
kOtusin <sejreg2@gmail.com>
2025-04-07 18:06:35 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+export PLATFORM_VERSION=12
+export ANDROID_MAJOR_VERSION=s
+export TARGET_SOC=s5e8825
+export LLVM_IAS=1
+
+[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b main build
+./build/build.sh "$@"
